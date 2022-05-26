8c160a026eece14c32648317e7942cff6a0391a2
Stepan <kryzhankov.stepan@mail.ru>
2022-05-26 18:38:06 +0300

    feat: tournament api endpoints models for tournament Ref #8

--- src/main/java/com/tech/tournaments/service/TournamentService.java
+++ src/main/java/com/tech/tournaments/service/TournamentService.java
@@ -1,10 +1,53 @@
 package com.tech.tournaments.service;
 
+import com.tech.tournaments.model.Tournament;
+import com.tech.tournaments.model.dto.TournamentDto;
+
+import java.util.UUID;
+
 public interface TournamentService {
     /**
-     * Test method
+     * Создание турнира
      *
-     * @return "Hello World" string
+     * @return ID созданного турнира
      */
-    String helloWorld();
+    UUID createNewTournament(TournamentDto tournamentDto);
+
+    /**
+     * Получение турнира по ИД
+     *
+     * @param id - ид турнира
+     * @return - информация о турнире
+     */
+    Tournament getTournamentById(UUID id);
+
+    /**
+     * Добавление команды в турнир по ИД
+     *
+     * @param id     - ид турнира
+     * @param teamId - ид команды
+     */
+    void addTeamInTournament(UUID id, UUID teamId);
+
+    /**
+     * Исключение команды из турнира по ИД
+     *
+     * @param id     - ид турнира
+     * @param teamId - ид команды
+     */
+    void removeTeamFromTournament(UUID id, UUID teamId);
+
+    /**
+     * Досрочное начало турнира
+     *
+     * @param id - ид турнира
+     */
+    void startTournament(UUID id);
+
+    /**
+     * Отмена турнира
+     *
+     * @param id - ид турнира
+     */
+    void cancelTournament(UUID id);
 }
