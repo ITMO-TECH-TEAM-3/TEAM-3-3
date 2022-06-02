01b28ef4eab1e5dd97d885e443d7aae732abd532
Daniil Muller <mullerdaniil@mail.ru>
2022-06-03 01:29:06 +0300

    feat(matches): add matches endpoints, services and repositories (#20)

---
+++ src/main/java/com/tech/tournaments/service/MatchService.java
@@ -0,0 +1,76 @@
+package com.tech.tournaments.service;
+
+import com.tech.tournaments.model.Match;
+import com.tech.tournaments.model.MatchResult;
+import com.tech.tournaments.model.dto.MatchResultDto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface MatchService {
+    /**
+     * Создание нового матча
+     *
+     * @param startDateTime - начало матча
+     * @param team1Id - первая команда
+     * @param team2Id - вторая команда
+     * @param round - круг
+     * @return - информация о матче
+     */
+    Match createNewMatch(LocalDateTime startDateTime,
+                         UUID team1Id,
+                         UUID team2Id,
+                         int round);
+
+    /**
+     * Получение матча по id
+     * @param id - id матча
+     * @return - информация о матче
+     */
+    Match getMatchById(UUID id);
+
+    /**
+     * Получение списка матчей, проводимых в данную дату
+     * @param date - дата проведения матчей
+     * @return - список матчей, проводимых в данную дату
+     */
+    List<Match> getMatchesByDate(LocalDate date);
+
+    /**
+     * Получение списка матчей, проводимых в данном турнире
+     * @param id - id турнира
+     * @return - список матчей, проводимых в данном турнире
+     */
+    List<Match> getMatchesByTournamentId(UUID id);
+
+    /**
+     * Получение результата по id результата
+     * @param id - id результата
+     * @return - информация о результате
+     */
+    MatchResult getResultById(UUID id);
+
+    /**
+     * Получение результата по id матча
+     * @param matchId - id матча
+     * @return - информация о результате матча
+     */
+    MatchResult getResultByMatchId(UUID matchId);
+
+    /**
+     * Начало матча
+     *
+     * @param id - id матча
+     */
+    void startMatch(UUID id);
+
+    /**
+     * Завершение матча
+     *
+     * @param id - id матча
+     * @param matchResultDto - информация о результате матча
+     */
+    void finishMatch(UUID id, MatchResultDto matchResultDto);
+}
---
+++ src/main/java/com/tech/tournaments/service/impl/MatchServiceImpl.java
@@ -0,0 +1,72 @@
+package com.tech.tournaments.service.impl;
+
+import com.tech.tournaments.model.Match;
+import com.tech.tournaments.model.MatchResult;
+import com.tech.tournaments.model.dto.MatchResultDto;
+import com.tech.tournaments.service.MatchService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class MatchServiceImpl implements MatchService {
+
+    @Override
+    public Match createNewMatch(LocalDateTime startDateTime, UUID team1Id, UUID team2Id, int round) {
+        LOG.info("Create a new match: {}, {}, {}, {}", startDateTime, team1Id, team2Id, round);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public Match getMatchById(UUID id) {
+        LOG.info("Get match by id {}", id);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public List<Match> getMatchesByDate(LocalDate date) {
+        LOG.info("Get matches by date {}", date);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public List<Match> getMatchesByTournamentId(UUID id) {
+        LOG.info("Get matches by tournament id {}", id);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public MatchResult getResultById(UUID id) {
+        LOG.info("Get result by id {}", id);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public MatchResult getResultByMatchId(UUID matchId) {
+        LOG.info("Get result by match id {}", matchId);
+        // todo: implement logic
+        return null;
+    }
+
+    @Override
+    public void startMatch(UUID id) {
+        LOG.info("Start a match {}", id);
+        // todo: implement logic
+    }
+
+    @Override
+    public void finishMatch(UUID id, MatchResultDto matchResultDto) {
+        LOG.info("Finish a match {}, {}", id, matchResultDto);
+        // todo: implement logic
+    }
+}
